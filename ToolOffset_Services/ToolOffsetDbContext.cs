a813f51d46e8054d3c251e25aa1f4ec2355cf8ac
Andrew VanderToorn <andrew.vandertoorn@gmail.com>
2020-03-15 08:48:51 -0400

    Added DbContext and changed some project configs

---
+++ ToolOffset_Services/ToolOffsetDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using ToolOffset_Models.Models;
+
+namespace ToolOffset_Services
+{
+    public class ToolOffsetDbContext : DbContext
+    {
+        public DbSet<ToolOffset> Offsets { get; set; }
+    }
+}
